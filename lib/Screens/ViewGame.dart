064c401f2aeb1d94afe03bdb03a23d47d90b8a9b
Shaoing <szhleon1010@gmail.com>
2025-10-21 17:02:06 -0700

    added click

--- lib/Screens/ViewGame.dart
+++ lib/Screens/ViewGame.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chess_board/flutter_chess_board.dart' as chess;
 import 'package:stockfish/stockfish.dart';
@@ -19,6 +20,7 @@ class _ViewGamePageState extends State<ViewGamePage> {
 
   List<String> Moves = [];
   List<String> History = [];
+  List<String> BestLines = [];
   int currentMove = 0;
   void  extractMovesFromPGN(String PGN){
     final chesspackage = chess.Chess();
@@ -35,19 +37,42 @@ class _ViewGamePageState extends State<ViewGamePage> {
         History.addAll(List.filled(actualMoves.length, ""));
       }
     }
+    History.add("");
     print(Moves);
   }
-  void doNextMove(){
-    if(currentMove < Moves.length){
+  void doNextMove(bool calculate) {
+    if (currentMove < Moves.length) {
       final FENBoard = controller.getFen();
       History[currentMove] = FENBoard;
       controller.makeMoveWithNormalNotation(Moves[currentMove]);
       setState(() {
         currentMove += 1;
       });
-      stockfish.stdin = 'position fen ${controller.getFen()}';
-      stockfish.stdin = 'go movetime 1000';
+      if(currentMove == Moves.length){
+        History[currentMove] = controller.getFen();
+      }
+      if (calculate) {
+        stockfish.stdin = 'position fen ${controller.getFen()}';
+        stockfish.stdin = 'go depth 8';
+      }
     }
+  
+  }
+  Future<void> jumptomove(int index)async{
+    if(index + 1 < History.length&&History[index + 1] != ""){
+      controller.loadFen(History[index + 1 ]);
+    }
+    else{
+      for(int i = currentMove; i <= index; i++){
+        doNextMove(false);
+        await Future.delayed(Duration(microseconds: 100));
+      }
+    }
+    stockfish.stdin = 'position fen ${controller.getFen()}';
+    stockfish.stdin = 'go depth 8';
+    setState(() {
+      currentMove = index + 1 ;
+    });
   }
   void undoMove(){
     if(currentMove > 0){
@@ -56,7 +81,7 @@ class _ViewGamePageState extends State<ViewGamePage> {
       });
       controller.loadFen(History[currentMove]);
       stockfish.stdin = 'position fen ${controller.getFen()}';
-      stockfish.stdin = 'go movetime 1000';
+      stockfish.stdin = 'go depth 8';
     }
   }
   Future<void> initializestockfish()async{
@@ -74,21 +99,31 @@ class _ViewGamePageState extends State<ViewGamePage> {
           isLoading = false;
         });
       }
+      else if(event.startsWith("info depth 8")){
+        final lineString = event.substring(event.indexOf(' pv')+4);
+        BestLines.add(lineString);
+      }
       else{
         setState(() {
           isLoading = true;
+          BestLines = [];
         });
       }
     });
     stockfish.stdin = 'isready';
     stockfish.stdin = 'position startpos';
-    stockfish.stdin = 'go movetime 1000';
+    stockfish.stdin = 'setoption name MultiPV value 3';
+    stockfish.stdin = 'go depth 8';
   }
   @override
   void initState(){
     super.initState();
     extractMovesFromPGN(widget.gameString);
     initializestockfish();
+    controller.addListener((){
+      stockfish.stdin = 'position fen ${controller.getFen()}';
+      stockfish.stdin = 'go depth 8';
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -102,7 +137,7 @@ class _ViewGamePageState extends State<ViewGamePage> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(onPressed: ()=>undoMove(), child: Icon(Icons.arrow_circle_left, size: 30,)),
-                ElevatedButton(onPressed: ()=> doNextMove(), child: Icon(Icons.arrow_circle_right, size: 30,))
+                ElevatedButton(onPressed: ()=> doNextMove(true), child: Icon(Icons.arrow_circle_right, size: 30,))
               ],
             ),
             Expanded(
@@ -120,11 +155,23 @@ class _ViewGamePageState extends State<ViewGamePage> {
                             color: index == currentMove - 1 ? Colors.green : Colors.white,
                             fontWeight: index == currentMove - 1 ? FontWeight.bold : FontWeight.normal,
                           ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => jumptomove(index)
                         );
                       }).toList(),
                     )
                 )
             ),
+            SizedBox(
+            height: 80,
+            child: (!isLoading)?Column(
+              children: [
+                Text(BestLines[0]),
+                Text(BestLines[1]),
+                Text(BestLines[2]),
+              ],
+            ):SizedBox(),
+          ),
           Text("Best Move:$BestMove"),
             SizedBox(
               height: 50,
